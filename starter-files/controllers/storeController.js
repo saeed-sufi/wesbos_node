3da0b4effa5b6b8c8ff7d75605051b0926a78567
s.sufi <saeed.sufi@hotmail.com>
2024-02-12 15:12:53 +0330

    Implemented saving newly added stores to the store db

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,13 +1,19 @@
+const pool = require('../dbConfig')
+
 exports.homePage = (req, res) => {
 
   res.render('index')
 }
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title:  '➕ Add Store' })
+  res.render('editStore', { title: '➕ Add Store' })
 }
 
-exports.createStore = (req, res) => {
-  res.json(req.body)
-}
+exports.createStore = async (req, res) => {
+  // res.json(req.body)
+  req.body.tags = Array.isArray(req.body.tags) ? req.body.tags : [req.body.tags]
 
+  await pool.query(`INSERT INTO store (name, slug, description, tags) VALUES ($1, $2, $3, $4)`, [req.body.name, req.body.name, req.body.description, req.body.tags])
+
+  res.redirect('/')
+}
